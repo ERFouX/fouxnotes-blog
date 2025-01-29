65240339d012d12a0de7bc50a02fa5e4410f7603
ERFouX <ERFouX@gmail.com>
2025-01-29 13:08:20 +0330

    Create about view extending layout.blade.php

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title', 'FouX Notes | About')
+
+@section('content')
+<div class="blog-header">
+    <h2>About</h2>
+</div>
+<div class="blog-container">
+    <div class="blog-card">
+        <h2>ERFouX</h2>
+        <p>A lonely nerd, dreaming of voice acting and dubbing, loving Linux and free software, music and chess lover; also eager to learn new things.</p>
+    </div>
+</div>
+@endsection
