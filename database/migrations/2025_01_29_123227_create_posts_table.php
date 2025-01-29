0ba2594b9880c69ef1fb45b434e0e4b8ab96747a
ERFouX <ERFouX@gmail.com>
2025-01-29 20:49:54 +0330

    Add migration for creating posts table with user relationship and necessary fields

---
+++ database/migrations/2025_01_29_123227_create_posts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('category');
+            $table->string('title');
+            $table->string('banner')->nullable();
+            $table->date('date');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
