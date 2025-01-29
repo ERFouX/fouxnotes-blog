3d80e9c4ccc621f950d84a65774fae332ae361ce
ERFouX <ERFouX@gmail.com>
2025-01-29 13:33:40 +0330

    Updated header and added a message to the 404 page

--- resources/views/notfound.blade.php
+++ resources/views/notfound.blade.php
@@ -1,35 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('../css/main.css') }}">
-    <title>FouX Notes | NotFound</title>
-</head>
-<body>
-    <div class="sb">
-    <a href="{{ route('home') }}">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
-    </div>
+@extends('layout')
 
-    <div class="sb-container">
-        <div class="blog-header">
-            <h2>Blog Name</h2>
-        </div>
-        <div class="blog-container">
-            <div class="blog-card">
-                <div class="notfound">
-                    <h1>404 | Not Found</h1>
-                    <a href="{{ route('home') }}">Back To Home</a>
-                </div>
+@section('title', 'FouX Notes | Not Found (404)')
+
+@section('additional-css')
+    <link rel="stylesheet" href="{{ asset('../css/login.css') }}">
+@endsection
+
+@section('content')
+    <div class="blog-header">
+        <h2>404 | Not Found</h1>
+    </div>
+    
+    <div class="blog-container">
+        <div class="blog-card">
+            <div class="notfound">
+                <h1>Oops! Looks like you're lost in the digital wilderness!</h1>
+                <p>Don't worry, even the best explorers get lost sometimes. <a href="{{ route('home') }}">Find your way back home!</a></p>
             </div>
         </div>
-        <div class="footer">
-            <h2>Footer</h2>
-            <p>This was inspired by W3 Schools.</p>
-        </div>
     </div>
-</body>
-</html>
+@endsection
