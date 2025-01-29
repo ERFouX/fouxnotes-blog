adc644089e03df683ba60407603d9cd7a32f1c34
ERFouX <ERFouX@gmail.com>
2025-01-29 12:58:01 +0330

    Add layout.blade.php for reusable layout and reduced duplication

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="{{ asset('../css/main.css') }}">
+    <!-- Additional Css Links-->
+    @yield('additional-css')
+    <!---->
+    <title>@yield('title')</title>
+</head>
+<body>
+<div class="sb">
+    <a class="{{ $active === 'home' ? 'sb-active' : '' }}" href="{{ route('home') }}">Home</a>
+    
+    @guest
+        <a class="{{ $active === 'login' ? 'sb-active' : '' }}" href="{{ route('login') }}">Login</a>
+        <a class="{{ $active === 'register' ? 'sb-active' : '' }}" href="{{ route('register') }}">Register</a>
+    @endguest
+
+    @auth
+        <a class="{{ $active === 'create-post' ? 'sb-active' : '' }}" href="{{ route('create.post') }}">Create Post</a>
+    @endauth
+
+    <a class="{{ $active === 'about' ? 'sb-active' : '' }}" href="{{ route('about') }}">About</a>
+    
+    <div class="spacer"><!-- Spacer to push logout button to the bottom --></div>
+    
+    @auth
+        <button class="logout-button" onclick="confirmLogout()">
+            Logout
+        </button>
+    @endauth
+</div>
+
+
+
+    <script>
+        function confirmLogout() {
+            if (confirm("Are you sure you want to log out?")) {
+                document.querySelector('form#logout-form').submit();
+            }
+        }
+    </script>
+
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
+
+<div class="sb-container">
+<!-- Alerts -->
+@if (session('successful'))
+    <div class="alert alert-success">
+        {{ session('successful') }}
+    </div>
+@endif
+
+@if (session('logged_out'))
+    <div class="alert alert-danger">
+        {{ session('logged_out') }}
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<!---->
+
+
+    <!-- Page Content -->
+    @yield('content')
+    <!---->
+
+    <!-- Footer -->
+    <div class="footer">
+        <h2>Footer</h2>
+        <p>Made with love by <a href="{{ route('about') }}">ERFouX</a></p>
+    </div>
+</div>
+</body>
+</html>
