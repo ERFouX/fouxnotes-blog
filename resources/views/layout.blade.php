4c9b6efa2cd0b863a599a01bee7aab27342dee66
ERFouX <ERFouX@gmail.com>
2025-02-26 04:00:16 +0330

    Add Saved tab for viewers

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,77 +11,77 @@
     <title>@yield('title')</title>
 </head>
 <body>
-<div class="sb">
-    <a class="{{ $active === 'home' ? 'sb-active' : '' }}" href="{{ route('home') }}">Home</a>
-    
-    @guest
-        <a class="{{ $active === 'login' ? 'sb-active' : '' }}" href="{{ route('login') }}">Login</a>
-        <a class="{{ $active === 'register' ? 'sb-active' : '' }}" href="{{ route('register') }}">Register</a>
-    @endguest
-
-    @auth
-        <a class="{{ $active === 'dashboard' ? 'sb-active' : '' }}" href="{{ route('dashboard') }}">Dashboard</a>
+    <div class="sb">
+        <a class="{{ $active === 'home' ? 'sb-active' : '' }}" href="{{ route('home') }}">Home</a>
         
-        <!-- Only show these links when on the dashboard -->
-        @if ($active === 'dashboard')
-            <a class="{{ $active === 'posts' ? 'sb-active' : '' }}" href="{{ route('posts.index') }}">Posts</a>
-            <a class="{{ $active === 'categories' ? 'sb-active' : '' }}" href="{{ route('categories.index') }}">Categories</a>
-            <a class="{{ $active === 'settings' ? 'sb-active' : '' }}" href="{{ route('settings.index') }}">Settings</a>
-        @endif
-    @endauth
+        @guest
+            <a class="{{ $active === 'login' ? 'sb-active' : '' }}" href="{{ route('login') }}">Login</a>
+            <a class="{{ $active === 'register' ? 'sb-active' : '' }}" href="{{ route('register') }}">Register</a>
+        @endguest
 
-    <a class="{{ $active === 'about' ? 'sb-active' : '' }}" href="{{ route('about') }}">About</a>
-    
-    <div class="spacer"><!-- Spacer to push logout button to the bottom --></div>
-    
-    @auth
-        <button class="logout-button" onclick="confirmLogout()">
-            Logout ({{ Auth::user()->username }})
-        </button>
-    @endauth
-</div>
+        @auth
+            @if (Auth::user()->role === 'author')
+                <a class="{{ $active === 'dashboard' ? 'sb-active' : '' }}" href="{{ route('dashboard') }}">Dashboard</a>
+                
+                <!-- Only show these links when on the dashboard -->
+                @if ($active === 'dashboard')
+                    <a class="{{ $active === 'posts' ? 'sb-active' : '' }}" href="{{ route('posts.index') }}">◽ Posts</a>
+                    <a class="{{ $active === 'categories' ? 'sb-active' : '' }}" href="#">◽ Categories</a>
+                    <a class="{{ $active === 'settings' ? 'sb-active' : '' }}" href="#">◽ Settings</a>
+                @endif
+            @elseif (Auth::user()->role === 'viewer')
+                <a class="{{ $active === 'saved' ? 'sb-active' : '' }}" href="#">Saved</a>
+            @endif
+        @endauth
 
-<script>
-    function confirmLogout() {
-        if (confirm("Are you sure you want to log out?")) {
-            document.querySelector('form#logout-form').submit();
+        <a class="{{ $active === 'about' ? 'sb-active' : '' }}" href="{{ route('about') }}">About</a>
+
+        <div class="spacer"><!-- Spacer to push logout button to the bottom --></div>
+        
+        @auth
+            <button class="logout-button" onclick="confirmLogout()">
+                Logout ({{ Auth::user()->username }})
+            </button>
+        @endauth
+    </div>
+
+    <!-- Logout Warning Script -->
+    <script>
+        function confirmLogout() {
+            if (confirm("Are you sure you want to log out?")) {
+                document.querySelector('form#logout-form').submit();
+            }
         }
-    }
-</script>
+    </script>
+    <!-- Logout Action -->
+    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+        @csrf
+    </form>
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    @csrf
-</form>
-
-
-<div class="sb-container">
-<!-- Alerts -->
-@if (session('successful'))
-<div class="alert alert-success">
-        {{ session('successful') }} <b>{{ Auth::user()->username }}</b>
-    </div>
-@endif
-
-@if (session('logged_out'))
-    <div class="alert alert-danger">
-        {{ session('logged_out') }}
-    </div>
-@endif
-
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<!---->
+    <!-- Alerts -->
+    <div class="sb-container">
+    @if (session('successful'))
+    <div class="alert alert-success">
+            {{ session('successful') }} <b>{{ Auth::user()->username }}</b>
+        </div>
+    @endif
+    @if (session('logged_out'))
+        <div class="alert alert-danger">
+            {{ session('logged_out') }}
+        </div>
+    @endif
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     <!-- Page Content -->
     @yield('content')
-    <!---->
 
     <!-- Footer -->
     <div class="footer">
