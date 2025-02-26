bc940fe6517538f07f6b88bec5edc9c8e9053621
ERFouX <ERFouX@gmail.com>
2025-02-26 03:59:03 +0330

    Add role to users table

--- database/migrations/2025_01_26_184813_create_users_table.php
+++ database/migrations/2025_01_26_184813_create_users_table.php
@@ -13,6 +13,7 @@ class CreateUsersTable extends Migration
             $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('viewer');
             $table->rememberToken();
             $table->timestamps();
         });
